9cd457080f468491a8ad621df7b981e11a6daf35
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-07-05 17:11:45 +0200

    Impostato progetto iniziale

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('guest.index');
+})->name('home');;
+
+Auth::routes();
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->prefix('admin')
+    ->name('admin.')
+    ->group( function(){
+        Route::get('/', 'HomeController@index')->name('index');
+
+    });
